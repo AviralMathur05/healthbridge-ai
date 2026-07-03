710735679a2162494567584c6804769502ceba23
Aviral Mathur <aviral.mathur2020@gmail.com>
2026-07-04 01:18:28 +0530

    Fix CORS

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://healthbridge-ai.onrender.com/"
+  baseURL: "https://healthbridge-ai.onrender.com"
 });
 
 export default api;
